16fb121d5191f9ab6b908465725003accbd848d7
edilenelopes <lopes_edi@yahoo.com.br>
2021-02-05 14:59:30 -0300

    altera o schema maes para ter senha

--- src/controllers/maes-controllers.js
+++ src/controllers/maes-controllers.js
@@ -1,4 +1,3 @@
-const { response } = require('express')
 const express = require('express')
 const maesCollection = require('../models/maes-schema')
 
--- src/models/maes-schema.js
+++ src/models/maes-schema.js
@@ -4,16 +4,25 @@ const Schema = mongoose.Schema
 const maeSchema = new Schema ({
     nome: {
         type: String,
+        maxlength: 50,
         required: true
     },
     email: {
         type: String,
-        required: true
+        maxlength: 30,
+        unique: true,
+        required: true,
+        lowercase: true
     },
     celular: {
         type: Number,
         required: true
     },
+    password:{
+        type: String,
+        required: true,
+        select: false  //para não aparecer em nunhuma listagem
+    }
     
 }, { timestamps: true });
 
